387d67cae2ef5b0df9d4c1089813037b56104c64
MikeB1124 <balianmichael@gmail.com>
2023-05-06 11:11:26 -0700

    only to neighbors

--- src/clientMessages.py
+++ src/clientMessages.py
@@ -1,35 +1,41 @@
 from src.utils import *
 import socket
 import config
+from src.readFiles import *
 
 def readyToReceiveMessage():
     primaryNode = config.var["primary"]["primaryNode"]
     nodeList = config.var["servers"]["nodeList"]
     for node in nodeList:
-        if node["enabled"] == True or node["enabled"] == 0:
-            try:
-                id = str(node["id"])
-                if primaryNode["cost"][id]["pathCost"] != "inf":
-                    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    clientSocket.connect((node["ip"], int(node["port"])))
-                    message = f'Ready:{primaryNode["id"]}:{primaryNode["ip"]}:{primaryNode["port"]}:Ready for packets'
-                    clientSocket.send(message.encode())
-                    
-                    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    clientSocket.connect((node["ip"], int(node["port"])))
-                    message = f'RoutingTable|{primaryNode["id"]}|{primaryNode["ip"]}|{primaryNode["port"]}|{primaryNode["cost"]}'
-                    clientSocket.send(message.encode())
-            except:
-                continue
+        neighbors = readInitialCosts(config.var["settings"]["filename"], config.var["settings"]["numServers"], config.var["settings"]["numEdges"], primaryNode)
+        if neighbors[str(node["id"])]["pathCost"] != "inf":
+            # print(neighbors)
+            if node["enabled"] == True or node["enabled"] == 0:
+                try:
+                    id = str(node["id"])
+                    if primaryNode["cost"][id]["pathCost"] != "inf":
+                        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        clientSocket.connect((node["ip"], int(node["port"])))
+                        message = f'Ready:{primaryNode["id"]}:{primaryNode["ip"]}:{primaryNode["port"]}:Ready for packets'
+                        clientSocket.send(message.encode())
+                        
+                        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        clientSocket.connect((node["ip"], int(node["port"])))
+                        message = f'RoutingTable|{primaryNode["id"]}|{primaryNode["ip"]}|{primaryNode["port"]}|{primaryNode["cost"]}'
+                        clientSocket.send(message.encode())
+                except:
+                    continue
         
 
 def disableConnection(id):
     primaryNode = config.var["primary"]["primaryNode"]
     nodeList = config.var["servers"]["nodeList"]
+    neighbors = readInitialCosts(config.var["settings"]["filename"], config.var["settings"]["numServers"], config.var["settings"]["numEdges"], primaryNode)
     ip = ""
     port = 0
+    
     for node in nodeList:
-        if node["id"] == int(id):
+        if node["id"] == int(id) and neighbors[str(node["id"])]["pathCost"] != "inf":
             ip = node["ip"]
             port = node["port"]
             node["enabled"] = False
@@ -46,3 +52,5 @@ def disableConnection(id):
             clientSocket.send(message.encode())
         except:
             print(f'Server {id} not connected.')
+    else:
+        print("Server ID is not the neighbor.")
